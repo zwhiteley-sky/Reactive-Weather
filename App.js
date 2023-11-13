54269ed187535747621d0c2476124dce8713eb0e
Zachary Whiteley <zachary.whiteley@sky.uk>
2023-11-13 10:38:12 +0000

    Complete day one
     * Complete WeatherCard component.
     * Convert cities array in data.js to WeatherCard components.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// Import data and WeatherCard here
-
+import cities from "./data";
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
     return (
@@ -8,10 +8,11 @@ function App() {
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
-                {/* Render components here */}
-                
-
-
+                {
+                    cities.map(city => (
+                        <WeatherCard key={city.city} name={city.city} temperature={city.temperature} conditions={city.forecast} />
+                    ))
+                }
             </div>
             
         </>
--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,20 +1,17 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg"
 
-
-function WeatherCard() {
+export default function WeatherCard({ name, temperature, conditions }) {
   return (
     <div className = "card">
         <div className = "img-container">
             <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
-            <h3 className="card-title">CITY DATA HERE</h3>
-            <h5 className="card-text">TEMPERATURE DATA HERE</h5>
-            <h5 className="card-text">FORECAST DATA HERE</h5>
+            <h3 className="card-title">{ name }</h3>
+            <h5 className="card-text">{ temperature }</h5>
+            <h5 className="card-text">{ conditions }</h5>
         </div>
     </div>
   );
 };
-
-// Export the WeatherCard
